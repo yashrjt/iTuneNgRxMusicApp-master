4a229e4bb26b64235fb9bf900384d4332bac04b0
Yash <yasodha@rjtcompuquest.com>
2018-10-31 15:03:37 -0500

    Add sharedModule and add directories for Directives,components,Pipes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule } from '@angular/platform-browser';
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
+import {AppComponent } from './app.component';
+import {environment } from '../environments/environment';
 import {EffectsModule} from '@ngrx/effects';
 import {HttpClientModule} from '@angular/common/http';
 import {MusicService} from './music/services/music.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MusicModule} from './music/music.module';
-import { CartComponent } from './cart/cart/cart.component';
+import {CartComponent } from './cart/cart/cart.component';
 import {CartModule} from './cart/cart.module';
-import { CheckoutComponent } from './cart/checkout/checkout.component';
+import {CheckoutComponent } from './cart/checkout/checkout.component';
 import {AuthModule} from './auth/auth.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { StoreModule} from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {StoreRouterConnectingModule , RouterStateSerializer} from '@ngrx/router-store';
 import {metaReducers, reducers, CustomSerializer} from './store/reducer';
 import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
@@ -33,6 +34,7 @@ import {CoreModule} from './core/core.module';
     CartModule,
     AuthModule,
     CoreModule,
+    SharedModule,
     NgbModule.forRoot(),
     ReactiveFormsModule,
     StoreModule.forRoot(reducers, { metaReducers }),
